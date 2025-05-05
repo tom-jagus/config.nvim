72bb74da1d1a5b502ad01df201f5e71daa1bd4dc
Tom Jagus <168629725+tom-jagus@users.noreply.github.com>
2025-05-05 23:30:46 +0200

    Disable <leader>e keymap within snacks

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -2,6 +2,9 @@ return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   {
     "folke/snacks.nvim",
+    keys = {
+      { "<leader>e", false },
+    },
     opts = {
       explorer = { enabled = false },
     },

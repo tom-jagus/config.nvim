d3161dc0ed42cf850e1d11d26d0199f083ea7fa9
Tom Jagus <168629725+tom-jagus@users.noreply.github.com>
2025-05-05 23:55:50 +0200

    Add build function utilizing npx if available

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,31 +1,19 @@
 return {
-  -- -- install without yarn or npm
-  -- {
-  --     "iamcco/markdown-preview.nvim",
-  --     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  --     ft = { "markdown" },
-  --     build = function() vim.fn["mkdp#util#install"]() end,
-  -- },
-  --
-  -- -- install with yarn or npm
-  -- {
-  --   "iamcco/markdown-preview.nvim",
-  --   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  --   build = "cd app && yarn install",
-  --   init = function()
-  --     vim.g.mkdp_filetypes = { "markdown" }
-  --   end,
-  --   ft = { "markdown" },
-  -- },
-  --
-  -- -- install with npm
-  -- {
-  --   "iamcco/markdown-preview.nvim",
-  --   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  --   build = "cd app && npm install",
-  --   init = function()
-  --     vim.g.mkdp_filetypes = { "markdown" }
-  --   end,
-  --   ft = { "markdown" },
-  -- },
+  -- Install markdown preview, use npx if available.
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  ft = { "markdown" },
+  build = function(plugin)
+    if vim.fn.executable("npx") then
+      vim.cmd("!cd " .. plugin.dir .. " && cd app && npx --yes yarn install")
+    else
+      vim.cmd([[Lazy load markdown-preview.nvim]])
+      vim.fn["mkdp#util#install"]()
+    end
+  end,
+  init = function()
+    if vim.fn.executable("npx") then
+      vim.g.mkdp_filetypes = { "markdown" }
+    end
+  end,
 }

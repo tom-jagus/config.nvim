bc9f4eb412e57bcb65656795b14394dcd4372a27
Tom Jagus <168629725+tom-jagus@users.noreply.github.com>
2025-06-02 18:19:19 +0200

    Modify keymaps

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -58,30 +58,40 @@ return {
       highlight_overrides = {
         all = function(color)
           return {
+            -- Line number pane
+            LineNr = { fg = color.overlay2 },
+            CursorLineNr = { fg = color.yellow },
+            -- H1
             rainbow1 = { fg = color.red },
             Headline1 = { fg = color.red },
             RenderMarkdownH1 = { fg = color.red },
             RenderMarkdownH1Bg = { bg = "#47292d" }, --red
+            -- H2
             rainbow2 = { fg = color.green },
             Headline2 = { fg = color.green },
             RenderMarkdownH2 = { fg = color.green },
             RenderMarkdownH2Bg = { bg = "#32412d" }, --green
+            -- H3
             rainbow3 = { fg = color.blue },
             Headline3 = { fg = color.blue },
             RenderMarkdownH3 = { fg = color.blue },
             RenderMarkdownH3Bg = { bg = "#36394c" }, --blue
+            -- H4
             rainbow4 = { fg = color.teal },
             Headline4 = { fg = color.teal },
             RenderMarkdownH4 = { fg = color.teal },
             RenderMarkdownH4Bg = { bg = "#233c47" }, --teal
+            -- H5
             rainbow5 = { fg = color.yellow },
             Headline5 = { fg = color.yellow },
             RenderMarkdownH5 = { fg = color.yellow },
             RenderMarkdownH5Bg = { bg = "#474030" }, --yellow
+            -- H6
             rainbow6 = { fg = color.peach },
             Headline6 = { fg = color.peach },
             RenderMarkdownH6 = { fg = color.peach },
             RenderMarkdownH6Bg = { bg = "#4a3326" }, --peach
+            -- Other
             RenderMarkdownLink = { fg = color.sapphire },
             RenderMarkdownLinkText = { fg = color.sapphire },
             RenderMarkdownBullet = { fg = color.maroon },
